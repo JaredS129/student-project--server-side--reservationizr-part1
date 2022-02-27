2ecefac17ae270ebb8e9519a68b09647ae96b926
Jared Stevenson <jaredstevenson@developersinstitute.co.nz>
2022-02-28 11:41:44 +1300

    add get /reservations test to app.js and add test:watch script to client package.json

--- server/src/app.test.js
+++ server/src/app.test.js
@@ -1,58 +1,27 @@
-const { TestWatcher } = require("@jest/core");
 const request = require("supertest");
 const app = require("./app");
 
 describe("app", () => {
-  test("GET /properties should respond with a list of properties", async () => {
+  test("GET /reservations should respond with a list of reservations", async () => {
     const expected = [
       {
-        id: "61480db44ab0cf7175467757",
-        askingPrice: "$891822.26",
-        description:
-          "An easy living, conveniently located, brick & tile home on a highly desirable street and surrounded by quality homes.",
-        address: "8 Shasta Pass",
-        title: "A Beauty on Shasta",
-        img: "https://placeimg.com/640/480/arch",
+        id: "507f1f77bcf86cd799439011",
+        partySize: 4,
+        date: "2023-11-17T06:30:00.000Z",
+        userId: "614abe145f317b89a2e36883",
+        restaurantName: "Island Grill",
       },
       {
-        id: "61480db44ab0cf7175467755",
-        askingPrice: "$876330.57",
-        description: "Large Executive townhouse bordering On Town Centre",
-        address: "2 Bowman Avenue",
-        title: "Bowman Brilliance – Style and Value!",
-        img: "https://placeimg.com/642/482/arch",
-      },
-      {
-        id: "61480db44ab0cf7175467756",
-        askingPrice: "$946446.87",
-        description: "Combining contemporary comforts with a functional layout",
-        address: "8237 Moland Hill",
-        title: "Rare Moland Hill Stunner",
-        img: "https://placeimg.com/644/484/arch",
+        id: "614abf0a93e8e80ace792ac6",
+        partySize: 2,
+        date: "2023-12-03T07:00:00.000Z",
+        userId: "614abe145f317b89a2e36883",
+        restaurantName: "Green Curry",
       },
     ];
 
     await request(app)
-      .get("/properties")
-      .expect(200)
-      .expect((res) => {
-        expect(res.body).toEqual(expected);
-      });
-  });
-
-  test("GET /properties:id should respond with a single property", async () => {
-    const expected = {
-      id: "61480db44ab0cf7175467757",
-      askingPrice: "$891822.26",
-      description:
-        "An easy living, conveniently located, brick & tile home on a highly desirable street and surrounded by quality homes.",
-      address: "8 Shasta Pass",
-      title: "A Beauty on Shasta",
-      img: "https://placeimg.com/640/480/arch",
-    };
-
-    await request(app)
-      .get("/properties/61480db44ab0cf7175467757")
+      .get("/reservations")
       .expect(200)
       .expect((res) => {
         expect(res.body).toEqual(expected);
