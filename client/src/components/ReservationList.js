5d2095491c2b77b0f1ff90bc5525537d47d13b8e
Jared Stevenson <jaredstevenson@developersinstitute.co.nz>
2022-02-28 14:18:01 +1300

    fetch api data for reservationList.js in client

--- client/src/components/ReservationList.js
+++ client/src/components/ReservationList.js
@@ -1,10 +1,35 @@
 import "./ReservationList.css";
 import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 
 const ReservationList = () => {
+  const [reservations, setReservations] = useState([]);
+
+  const fetchData = async () => {
+    const response = await fetch("http://localhost:5001/reservations");
+    const data = await response.json();
+    setReservations(data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <>
       <h1>Upcoming reservations</h1>
+      <ul>
+        {reservations.map((reservation) => {
+          return (
+            <li key={reservation.id}>
+              <h2>{reservation.restaurantName}</h2>
+              <p>{reservation.date}</p>
+              <a href="*">View details</a>
+              <hr />
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 };
